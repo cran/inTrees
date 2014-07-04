f6379ae84374ccba6cbe39a151d5c8575b62b589
Houtao Deng <softwaredeng@gmail.com>
2014-07-04 00:00:00 +0000

    version 1.1

--- R/pruneRule.R
+++ R/pruneRule.R
@@ -1,10 +1,8 @@
 pruneRule <-
-function (rules, X, target) 
-{
-    newRuleMetric <- NULL
-    for (i in 1:nrow(rules)) {
-        newRuleMetric <- rbind(newRuleMetric, pruneSingleRule(rules[i, 
-            ], X, target))
-    }
-    return(newRuleMetric)
+function(rules,X,target, maxDecay = 0.05, typeDecay = 2){
+  newRuleMetric <- NULL
+  for(i in 1:nrow(rules)){
+    newRuleMetric <- rbind(newRuleMetric, pruneSingleRule(rules[i,],X,target, maxDecay, typeDecay))
+  }
+  return(newRuleMetric)
 }
--- R/pruneSingleRule.R
+++ R/pruneSingleRule.R
@@ -1,23 +1,33 @@
 pruneSingleRule <-
-function (rule, X, target, maxDecay = 0.05) 
-{
-    newRuleMetric <- measureRule(rule["condition"], X, target)
-    errOrig <- as.numeric(newRuleMetric["err"])
-    ruleV <- unlist(strsplit(rule["condition"], split = " & "))
-    pred <- rule["pred"]
-    if (length(ruleV) == 1) 
-        return(newRuleMetric)
-    for (i in length(ruleV):1) {
-        restRule <- ruleV[-i]
-        restRule <- paste(restRule, collapse = " & ")
-        metricTmp <- measureRule(restRule, X, target, pred)
-        errNew <- as.numeric(metricTmp["err"])
-        if ((errNew - errOrig)/max(errOrig, 1e-06) <= maxDecay) {
-            ruleV <- ruleV[-i]
-            newRuleMetric <- metricTmp
-            if (length(ruleV) <= 1) 
-                break
-        }
+function(rule, X, target, maxDecay, typeDecay){
+# typeDecay = 1: relative error increase; otherwise: absolute error increase
+
+  #A <- gregexpr("X\\[,[0-9]+\\]", s)
+  newRuleMetric <- measureRule(rule["condition"],X,target)
+  errOrig <- as.numeric(newRuleMetric["err"])  
+  ruleV <- unlist(strsplit(rule["condition"],split= " & "))
+  pred <- rule["pred"]
+  # newRule <- NULL
+  if(length(ruleV)==1) return(newRuleMetric)
+  for(i in length(ruleV):1){
+    restRule <- ruleV[-i]
+    restRule <- paste(restRule,collapse= " & ")
+    metricTmp <- measureRule(restRule,X,target,pred)
+    errNew <- as.numeric(metricTmp["err"]) 
+    if(typeDecay == 1){
+      decay <- (errNew-errOrig)/max(errOrig,0.000001)
+    }else{
+      decay <- (errNew-errOrig)
+    }      
+    if( decay <= maxDecay){
+    #if( errNew-errOrig <= maxDecay){
+      ruleV <- ruleV[-i] 
+      # newRule saves the last changed rule and metrics
+      newRuleMetric <- metricTmp
+      if(length(ruleV)<=1)break
     }
-    return(newRuleMetric)
+  }
+  return(newRuleMetric)
+  #rule["condition"] <- paste(ruleV,collapse= " & ")
+  #return(rule)
 }
